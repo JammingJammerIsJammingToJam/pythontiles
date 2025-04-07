5cea3fa3823a17684b77991d1c171d5ee5205ed9
JammingJammerIsJammingToJam <22knowlson@parmiters.herts.sch.uk>
2025-04-07 19:12:12 +0100

    Update board.py

--- board.py
+++ board.py
@@ -1,17 +1,35 @@
+import nmedecode as nme
+
 class Board:
-    #Setting Up Board
-    def __init__(self, height, width):
+    # Setting Up Board
+    def __init__(self):
+        self.height = None
+        self.width = None
+        self.board = None
+
+    def board_size(self, height, width):
         self.height = height
         self.width = width
-        self.board = [["empty" for j in range(0, height)] for i in range(0, width)]
+
+    def reset_to_empty(self):
+        self.board = [["empty" for j in range(0, self.height)] for i in range(0, self.width)]
+
+    def import_from_nme(self, filename):
+        file = nme.NME_Decode_File(filename)
+        self.board = file
+        self.board_size(len(file), len(file[0]))
+
+    #Information Checks
     def value_at_tile(self, x, y):
-        return self.board[y-1][x-1]
+        return self.board[y - 1][x - 1]
+
     def is_legal_tile(self, x, y):
         return not (x < 1 or y < 1 or x > self.width or y > self.height)
-    
-    #Moving Pieces
+
+    # Moving Pieces
     def change_tile(self, x, y, text):
-        self.board[y-1][x-1] = text
+        self.board[y - 1][x - 1] = text
+
     def move_piece(self, x1, y1, x2, y2):
         if not self.is_legal_tile(x1, y1) or not self.is_legal_tile(x2, y2):
             print("Error Not Legal Tile")
